8cdb0452be161d2f56f37f588d3b4438fb5d5a26
BrianMartin176743 <lily176743@gmail.com>
2023-09-14 15:15:57 +0900

    update commit

---
+++ .storybook/preview.jsx
@@ -0,0 +1,27 @@
+
+import { Global } from '@emotion/react';
+
+import { GlobalStyle } from '../src/shared/global';
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  decorators: [
+    (Story) => {
+      <>
+        <Global styles={GlobalStyle} />
+        <Story />
+      </>
+    },
+  ],
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
